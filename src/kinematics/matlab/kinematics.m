a5f723bef236e7961ccc3edfb75e4c9c2dfd46cc
Alexios Karadimos <karadalex@gmail.com>
2020-03-02 17:37:40 +0200

    quick calculations with matlab

--- src/kinematics/matlab/kinematics.m
+++ src/kinematics/matlab/kinematics.m
@@ -2,4 +2,16 @@ L = zeros(1,7);
 d = [0.36, 0, 0.42, 0, 0.4, 0, 0];
 a = [0, -pi/2, pi/2, -pi/2, pi/2, -pi/2, pi/2];
 
-[M, M_joints] = fwdKinSym(L(1:6), d(1:6), a(1:6));
+[M, M_joints] = fwdKinSym(L(2:7), d(2:7), a(2:7));
+
+M_0_3 = eye(4);
+for i=1:3
+    M_0_3 = M_0_3 * M_joints(:,:,i);
+end
+p = M_0_3 * M_joints(:,4,4);
+
+R_3_6 = eye(4);
+for i=4:6
+    R_3_6 = R_3_6 * M_joints(:,:,i);
+end
+R_3_6 = R_3_6(1:3,1:3);
