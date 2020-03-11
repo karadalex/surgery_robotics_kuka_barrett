c487344e39239f5dd44ae64a7df1cc8e2a383bd1
Alexios Karadimos <karadalex@gmail.com>
2020-03-11 15:12:52 +0200

    Simple blue tool detection with OpenCV - VERY SLOW

--- src/vision/scripts/test1.py
+++ src/vision/scripts/test1.py
@@ -25,8 +25,33 @@ class image_converter:
       print(e)
 
     (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
+
+    numBluePixels = 0
+    for i in range(rows):
+      for j in range(cols):
+        if cv_image[i,j,0] >= 200:
+          numBluePixels += 1
+
+    if numBluePixels >= 20:
+      detectionMsg = "Blue tool detected"
+    else:
+      detectionMsg = "No tool detected"
+
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    topLeftCorner = (50,50)
+    fontScale              = 1
+    fontColor              = (255,255,255)
+    lineType               = 2
+
+    cv2.putText(
+      cv_image,
+      detectionMsg, 
+      topLeftCorner, 
+      font, 
+      fontScale,
+      fontColor,
+      lineType
+    )
 
     cv2.imshow("OpenCV Image", cv_image)
     cv2.waitKey(3)
@@ -35,15 +60,19 @@ class image_converter:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e:
       print(e)
+  
+
+
 
 def main(args):
   ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
+  rospy.init_node('opencv_image_view', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:
     print("Shutting down")
   cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(sys.argv)
