60e6b167ce4822092c3ff28c2b01924131cb4355
Alexios Karadimos <karadalex@gmail.com>
2020-03-11 14:38:28 +0200

    arm path without barret hand

--- src/kuka_barrett/src/joint_controller.cpp
+++ src/kuka_barrett/src/joint_controller.cpp
@@ -12,39 +12,47 @@ int main(int argc, char **argv)
 	// Set up ROS.
 	ros::init(argc, argv, "joint_controller");
 	ros::NodeHandle nh;
+	ros::Rate loop_rate(10);
 
 	// Barrett hand publishers
-	vector<ros::Publisher> bh_fingers_publishers;
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j11_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j12_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j13_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j21_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j22_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j23_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j31_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j32_position_controller/command",1000));
-	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j33_position_controller/command",1000));
+//	vector<ros::Publisher> bh_fingers_publishers;
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j11_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j12_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j13_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j21_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j22_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j23_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j31_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j32_position_controller/command",1000));
+//	bh_fingers_publishers.push_back(nh.advertise<std_msgs::Float64>("/bh_j33_position_controller/command",1000));
+
+	// Barrett hand command values
+//	vector<std_msgs::Float64> bh_msgs;
+//	vector<float> bh_angles;
+//	bh_angles = {0.2f, 1.7f, 2.0f, 0.9f, 1.7f, 2.0f, 0.9f, 1.7f, 2.0f};
+//	bh_angles.resize(9, 0);
+//	for (int i = 0; i < 9; ++i) {
+//		std_msgs::Float64 msg; msg.data = bh_angles[i];
+//		bh_msgs.push_back(msg);
+//	}
+
 
 	// KUKA iiwa LBR 14 arm controller publisher
 	ros::Publisher kuka_publisher = nh.advertise<trajectory_msgs::JointTrajectory > ("/arm_controller/command", 1000);
-
-	ros::Rate loop_rate(10);
-
-	// Barrett hand command values
-	vector<std_msgs::Float64> bh_msgs;
-	vector<float> bh_angles;
-	bh_angles = {0.2f, 1.7f, 2.0f, 0.9f, 1.7f, 2.0f, 0.9f, 1.7f, 2.0f};
-	for (int i = 0; i < 9; ++i) {
-		std_msgs::Float64 msg; msg.data = bh_angles[i];
-		bh_msgs.push_back(msg);
-	}
 
 	// Create path and solve Inverse Kinematics for each point in path
 	vector<Pose*> path;
 	vector<Iiwa14Inv*> pathSolutions;
 
-	path.push_back(new Pose(0,-0.88,1.5,M_PI/2,0,M_PI));
 	path.push_back(new Pose(0,-0.88,1.6,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0,-0.88,1.35,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0,-0.88,1.6,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0,0.88,1.6,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0,0.88,1.4,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0,0.88,1.6,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0.2,-0.88,1.6,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0.2,-0.88,1.4,M_PI/2,0,M_PI));
+	path.push_back(new Pose(0.2,-0.88,1.6,M_PI/2,0,M_PI));
 	path.push_back(new Pose(0,0,2.266,0,0,0));
 
 	// Build iiwa arm joint trajectory message
@@ -64,32 +72,35 @@ int main(int argc, char **argv)
 
 	kuka_msg.points.resize(path.size());
 
-	for (int pathIndex = 0; pathIndex < path.size(); ++pathIndex) {
-		pathSolutions.push_back(new Iiwa14Inv(path.at(pathIndex)));
 
-		kuka_msg.points[pathIndex].positions.resize(7);
-		for (int i = 0; i < 7; ++i) {
-			vecf angles = pathSolutions.at(pathIndex)->solutionSet[0]; // Select first solution of the solution set
-			kuka_msg.points[pathIndex].positions[i] = angles[i];
-		}
-
-		// Velocities
-		kuka_msg.points[pathIndex].velocities.resize(7);
-		kuka_msg.points[pathIndex].effort.resize(7);
-		for (size_t j = 0; j < 7; ++j)
-		{
-			kuka_msg.points[pathIndex].velocities[j]=0.0;
-			kuka_msg.points[pathIndex].effort[j] = 0.0;
-		}
-		// To be reached 1 second after starting along the trajectory
-		kuka_msg.points[pathIndex].time_from_start = ros::Duration(2*(pathIndex+1));
-	}
-
-
+	int pathIndex = 0;
 	while(ros::ok()){
-		for (int i = 0; i < 9; ++i) bh_fingers_publishers.at(i).publish(bh_msgs.at(i));
+		if (pathIndex < path.size()) {
+			// for (int i = 0; i < 9; ++i) bh_fingers_publishers.at(i).publish(bh_msgs.at(i));
 
-		kuka_publisher.publish(kuka_msg);
+			pathSolutions.push_back(new Iiwa14Inv(path.at(pathIndex)));
+
+			kuka_msg.points[pathIndex].positions.resize(7);
+			for (int i = 0; i < 7; ++i) {
+				vecf angles = pathSolutions.at(pathIndex)->solutionSet[0]; // Select first solution of the solution set
+				kuka_msg.points[pathIndex].positions[i] = angles[i];
+			}
+
+			// Velocities
+			kuka_msg.points[pathIndex].velocities.resize(7);
+			kuka_msg.points[pathIndex].effort.resize(7);
+			for (int j = 0; j < 7; ++j)
+			{
+				kuka_msg.points[pathIndex].velocities[j]=0.0;
+				kuka_msg.points[pathIndex].effort[j] = 0.0;
+			}
+			// To be reached 1 second after starting along the trajectory
+			kuka_msg.points[pathIndex].time_from_start = ros::Duration(3*(pathIndex+1));
+
+			kuka_publisher.publish(kuka_msg);
+
+			pathIndex++;
+		}
 
 		ros::spinOnce();
 		loop_rate.sleep();
