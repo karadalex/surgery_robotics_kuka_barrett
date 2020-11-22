bfd333317029ac7c11cba2460a774bef3b697fc1
Alexios Karadimos <karadalex@gmail.com>
2020-11-22 19:14:47 +0200

    improve vrep environment for quick prototyping

--- vrep/ik.py
+++ vrep/ik.py
@@ -1,25 +1,70 @@
 import sim
 import sys
 import ctypes as ct
-from math import pi
+from math import pi, cos, sin
+from time import sleep
 
 
-def main():  
-  position1 = [pi/2,0,0,0,0,0,0]
-  moveArmToJointsPosition(position1)
+def main():
+  samplesNum = 20
+  step = 1.0 / samplesNum
+  t = 0
+  for i in range(samplesNum):
+    x = 0.1 * cos(2*pi*t) + 0.75
+    y = 0.1 * sin(2*pi*t) - 0.125
+    z = 1.2
+    yaw = 0
+    pitch = pi/2
+    roll = 0
+    pose = [x, y, z] + eulerToQuaternion(yaw, pitch, roll)
+    moveTargetFrame(pose)
+    t = step*i
+    sleep(0.5)
 
 
-def moveArmToJointsPosition(targetPosition):
+def moveTargetFrame(targetPosition):
+  """[summary]
+
+  Args:
+      targetPosition ([type]): [description]
+  """
   emptyBuff = bytearray()
 
   res,retInts,target1Pose,retStrings,retBuffer = sim.simxCallScriptFunction(
     clientID,
-    'LBR_iiwa_14_R820',
+    'target',
     sim.sim_scripttype_childscript,
-    'commandIIWA',
+    'commandTarget',
     [],targetPosition,[],emptyBuff,
     sim.simx_opmode_oneshot_wait
   )
+  print(res,retInts,target1Pose,retStrings,retBuffer)
+
+
+def eulerToQuaternion(yaw, pitch, roll):
+  """[summary]
+
+  Args:
+      yaw ([type]): [description]
+      pitch ([type]): [description]
+      roll ([type]): [description]
+
+  Returns:
+      [type]: [description]
+  """
+  cy = cos(yaw * 0.5)
+  sy = sin(yaw * 0.5)
+  cp = cos(pitch * 0.5)
+  sp = sin(pitch * 0.5)
+  cr = cos(roll * 0.5)
+  sr = sin(roll * 0.5)
+
+  qw = cr * cp * cy + sr * sp * sy
+  qx = sr * cp * cy - cr * sp * sy
+  qy = cr * sp * cy + sr * cp * sy
+  qz = cr * cp * sy - sr * sp * cy
+
+  return [qx, qy, qz, qw]
 
 
 if __name__ == "__main__":
---
+++ vrep/target_controller.lua
@@ -0,0 +1,87 @@
+-- if you wish to execute code contained in an external file instead,
+-- use the require-directive, e.g.:
+--
+-- require 'myExternalFile'
+--
+-- Above will look for <V-REP executable path>/myExternalFile.lua or
+-- <V-REP executable path>/lua/myExternalFile.lua
+-- (the file can be opened in this editor with the popup menu over
+-- the file name)
+
+function sysCall_threadmain()
+  -- Put some initialization code here
+  simRemoteApi.start(19999)
+  robotFlag = false
+  targetPose = {0.5, -0.125, 2.2, 0, 0, 0, 1}
+
+  commandTarget = function(inInts,inFloats,inStrings,inBuffer)
+    robotFlag = true
+    if #inFloats == 7 then
+      targetPose = inFloats
+    end
+    return {},{},{'Robot received command'},'' -- return a string
+  end
+
+  -- Put your main loop here, e.g.:
+  --
+  while sim.getSimulationState() ~= sim.simulation_advancing_abouttostop do
+    if robotFlag then
+      moveTarget(targetPose)
+    end
+    sim.switchThread() -- resume in next simulation step
+  end
+end
+
+function sysCall_cleanup()
+  -- Put some clean-up code here
+end
+
+
+-- ADDITIONAL DETAILS:
+-- -------------------------------------------------------------------------
+-- If you wish to synchronize a threaded loop with each simulation pass,
+-- enable the explicit thread switching with 
+--
+-- sim.setThreadAutomaticSwitch(false)
+--
+-- then use
+--
+-- sim.switchThread()
+--
+-- When you want to resume execution in next simulation step (i.e. at t=t+dt)
+--
+-- sim.switchThread() can also be used normally, in order to not waste too much
+-- computation time in a given simulation step
+-- -------------------------------------------------------------------------
+
+function moveTarget(targetPose)
+  targetHandle = sim.getObjectHandle('target')
+  relativeToObjectHandle = -1
+  flags = -1
+
+  targetPosition = {targetPose[1], targetPose[2], targetPose[3]}
+  targetQuaternion = {targetPose[4], targetPose[5], targetPose[6], targetPose[7]}
+  print("targetPosition: ", targetPosition, "targetQuaternion: ", targetQuaternion)
+
+  vel = 0.2
+  accel = 2
+  jerk = 10
+  currentVel = {0,0,0,0}
+  currentAccel = {0,0,0,0}
+  maxVel = {vel, vel, vel, 1}
+  maxAccel = {accel, accel, accel, 1}
+  maxJerk = {jerk, jerk, jerk, 1}
+  targetVel = nil
+
+  -- sim.setObjectPosition(targetHandle, -1, targetPosition)
+  -- sim.setObjectOrientation(targetHandle, -1, targetQuaternion)
+  
+  result, newPos, newQuaternion, newVel, newAccel, timeLeft = sim.rmlMoveToPosition(
+    targetHandle, relativeToObjectHandle, 
+    flags, 
+    currentVel, currentAccel, 
+    maxVel, maxAccel, maxJerk,
+    targetPosition, targetQuaternion, targetVel
+  )
+  print(timeLeft)
+end
