35542cfd41d98b61dc7ee4885bea0dbc29966f33
Alexios Karadimos <karadalex@gmail.com>
2020-12-06 20:14:13 +0200

    Parameters docstring

--- src/trajectory/include/trajectory/CircleTrajectory.h
+++ src/trajectory/include/trajectory/CircleTrajectory.h
@@ -17,9 +17,21 @@ class CircleTrajectory {
 public:
 	vector<geometry_msgs::Pose> waypoints;
 
+	/**
+	 *
+	 * @param center
+	 * @param radius
+	 */
 	CircleTrajectory(Eigen::Vector3f center, float radius);
 
-	vector<geometry_msgs::Pose> getCartesianWaypoints(int samples);
+	/**
+	 *
+	 * @param samples
+	 * @param left_mat
+	 * @param right_mat
+	 * @return
+	 */
+	vector<geometry_msgs::Pose> getCartesianWaypoints(int samples, Eigen::Matrix4d left_mat = Eigen::Matrix4d::Identity(), Eigen::Matrix4d right_mat = Eigen::Matrix4d::Identity());
 
 private:
 	float xf0, yf0, zf0, r0;
