101041804b41b978b64b6d097d34764ea122a540
Alexios Karadimos <karadalex@gmail.com>
2020-12-19 23:43:51 +0200

    change planning parameters

--- src/kuka_barrett/src/robot_planner6.cpp
+++ src/kuka_barrett/src/robot_planner6.cpp
@@ -62,63 +62,14 @@ int main(int argc, char** argv)
 	ros::WallDuration(1.0).sleep();
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 	moveit::planning_interface::MoveGroupInterface move_group("iiwa_arm");
-	move_group.setPlanningTime(45.0);
+	move_group.setPlanningTime(0.5);
 	move_group.allowReplanning(true);
 	move_group.setNumPlanningAttempts(3);
+	// move_group.setPlannerId("RRTstar");
+	move_group.setGoalPositionTolerance(0.0005);
+	move_group.setGoalOrientationTolerance(0.0005);
 
 	ros::WallDuration(1.0).sleep();
-
-	// // Setup Move group
-	// static const std::string PLANNING_GROUP = "iiwa_arm";
-	// double pos_tolerance = 0.00005;
-	// double orient_tolerance = 0.00005;
-	// int plan_time_sec = 5;
-	// bool replanning = true;
-	// int plan_attempts = 6;
-	// const string base_frame = "world";
-	// TrajectoryExecution traj1 = TrajectoryExecution(PLANNING_GROUP, pos_tolerance, orient_tolerance, plan_time_sec, replanning, plan_attempts, base_frame);
-	//
-	// // X Y Z Roll Pitch Yaw
-	// vector<vector<float>> path1;
-	// // path.push_back({0, 0, 2.262, 0, 0, 0}); // For z >= 2.261 the robot reaches end of workspace, which is a signularity and cant be calculated from the numerical IK
-	// path1.push_back({0, 0, 2.26, 0, 0, 0}); // Home position
-	// // TCP position for point above cube
-	// path1.push_back({0.004149, -0.719461, 1.366577, 3.139543, 0.022613, -1.657719});
-	// traj1.executePath(path1);
-	//
-	// // Slowly approaching Cube
-	// // Move in a line segment while approaching cube
-	// geometry_msgs::Pose aboveCubePose = getPoseFromPathPoint(path1.at(path1.size()-1));
-	// std::vector<geometry_msgs::Pose> path2;
-	// path2.push_back(aboveCubePose);
-	// vector<float> onCubeFloatPose = {0.004149, -0.719461, 1.202148, 3.139543, 0.022613, -1.657719};
-	// geometry_msgs::Pose onCubePose = getPoseFromPathPoint(onCubeFloatPose);
-	// path2.push_back(onCubePose);
-	// traj1.executeCartesianPath(path2, "approaching cube");
-
-	// Create Collision Objects - Cube
-	std::vector<moveit_msgs::CollisionObject> collision_objects;
-	collision_objects.resize(1);
-
-	collision_objects[0].header.frame_id = "world";
-	collision_objects[0].id = "cube";
-
-/* Define the primitive and its dimensions. */
-	collision_objects[0].primitives.resize(1);
-	collision_objects[0].primitives[0].type = collision_objects[1].primitives[0].BOX;
-	collision_objects[0].primitives[0].dimensions.resize(3);
-	collision_objects[0].primitives[0].dimensions[0] = 0.1;
-	collision_objects[0].primitives[0].dimensions[1] = 0.1;
-	collision_objects[0].primitives[0].dimensions[2] = 0.1;
-
-/* Define the pose of the object. */
-	collision_objects[0].primitive_poses.resize(1);
-	collision_objects[0].primitive_poses[0].position.x = 0;
-	collision_objects[0].primitive_poses[0].position.y = -0.72;
-	collision_objects[0].primitive_poses[0].position.z = 1.0493;
-
-	collision_objects[0].operation = collision_objects[0].ADD;
-	planning_scene_interface.applyCollisionObjects(collision_objects);
 
 	// ****************************************************************************
 	// Pick Pipeline
@@ -129,19 +80,19 @@ int main(int argc, char** argv)
 	// Setting grasp pose
 	grasps[0].grasp_pose.header.frame_id = "world";
 	tf2::Quaternion orientation;
-	orientation.setRPY(3.139543, 0.022613, -1.657719);
+	orientation.setRPY(-M_PI, 0, M_PI_2);
 	grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-	grasps[0].grasp_pose.pose.position.x = 0.004149;
-	grasps[0].grasp_pose.pose.position.y = -0.719461;
-	grasps[0].grasp_pose.pose.position.z = 1.202148;
+	grasps[0].grasp_pose.pose.position.x = -0.610318;
+	grasps[0].grasp_pose.pose.position.y = -0.347105;
+	grasps[0].grasp_pose.pose.position.z = 1.175;
 
 	// Setting pre-grasp approach
 	// Defined with respect to frame_id
 	grasps[0].pre_grasp_approach.direction.header.frame_id = "world";
 	// Direction is set as positive x axis
 	grasps[0].pre_grasp_approach.direction.vector.z = -1.0;
-	grasps[0].pre_grasp_approach.min_distance = 0.05;
-	grasps[0].pre_grasp_approach.desired_distance = 0.1;
+	grasps[0].pre_grasp_approach.min_distance = 0.1;
+	grasps[0].pre_grasp_approach.desired_distance = 0.2;
 
 	// Setting post-grasp retreat
 	// Defined with respect to frame_id
@@ -149,7 +100,7 @@ int main(int argc, char** argv)
 	// Direction is set as positive z axis
 	grasps[0].post_grasp_retreat.direction.vector.z = 1.0;
 	grasps[0].post_grasp_retreat.min_distance = 0.1;
-	grasps[0].post_grasp_retreat.desired_distance = 0.25;
+	grasps[0].post_grasp_retreat.desired_distance = 0.2;
 
 	// Setting posture of eef before grasp
 	openGripper(grasps[0].pre_grasp_posture);
@@ -160,7 +111,7 @@ int main(int argc, char** argv)
 	// Set support surface as table1.
 	move_group.setSupportSurfaceName("table1");
 	// Call pick to pick up the object using the grasps given
-	move_group.pick("cube", grasps);
+	move_group.pick("surgical_tool1", grasps);
 
 	// ****************************************************************************
 	// Place Pipeline
@@ -173,9 +124,11 @@ int main(int argc, char** argv)
 	orientation.setRPY(0, 0, M_PI / 2);
 	place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
 	// While placing it is the exact location of the center of the object.
-	place_location[0].place_pose.pose.position.x = -0.08149;
-	place_location[0].place_pose.pose.position.y = 0.719461;
-	place_location[0].place_pose.pose.position.z = 1.202148;
+	place_location[0].place_pose.pose.position.x = 0.494890;
+	place_location[0].place_pose.pose.position.y = 0.465734;
+	place_location[0].place_pose.pose.position.z = 1.655359;
+	orientation.setRPY(-0.003199, -0.969072, 1.599642);
+	place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
 
 	// Setting pre-place approach
 	// Defined with respect to frame_id
@@ -183,7 +136,7 @@ int main(int argc, char** argv)
 	// Direction is set as negative z axis
 	place_location[0].pre_place_approach.direction.vector.z = -1.0;
 	place_location[0].pre_place_approach.min_distance = 0.1;
-	place_location[0].pre_place_approach.desired_distance = 0.5;
+	place_location[0].pre_place_approach.desired_distance = 0.2;
 
 	// Setting post-grasp retreat
 	// Defined with respect to frame_id
@@ -191,14 +144,14 @@ int main(int argc, char** argv)
 	// Direction is set as negative y axis
 	place_location[0].post_place_retreat.direction.vector.z = 1.0;
 	place_location[0].post_place_retreat.min_distance = 0.1;
-	place_location[0].post_place_retreat.desired_distance = 0.25;
+	place_location[0].post_place_retreat.desired_distance = 0.2;
 
 	// Setting posture of eef after placing object
 	// Similar to the pick case
 	openGripper(place_location[0].post_place_posture);
 
 	move_group.setSupportSurfaceName("table1");
-	move_group.place("cube", place_location);
+	move_group.place("surgical_tool1", place_location);
 
 	ros::shutdown();
 	return 0;
