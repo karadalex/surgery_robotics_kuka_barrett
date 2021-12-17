4439d8884bc22af1a0905b2557127f06b62b880d
Alexios Karadimos <karadalex@gmail.com>
2021-12-17 17:44:43 +0200

    Added extra point before tool insertion and improved logs

--- src/kuka_barrett/src/robot_planner2b.cpp
+++ src/kuka_barrett/src/robot_planner2b.cpp
@@ -27,27 +27,30 @@ int main(int argc, char** argv)
 	TrajectoryExecution traj1 = TrajectoryExecution(PLANNING_GROUP, pos_tolerance, orient_tolerance, plan_time_sec, replanning, plan_attempts, base_frame, plannerId);
 
 	// X Y Z Roll Pitch Yaw
-	vector<vector<float>> path1;
+	vector<vector<float>> float_path1;
 	// path.push_back({0, 0, 2.262, 0, 0, 0}); // For z >= 2.261 the robot reaches end of workspace, which is a signularity and cant be calculated from the numerical IK
-	path1.push_back({0, 0, 2.26, 0, 0, 0}); // Home position
+	float_path1.push_back({0, 0, 2.26, 0, 0, 0}); // Home position
 	// TCP position for point above fulcrum 1
-	path1.push_back({0.503454, 0.359961, 1.902053, -3.083623, 1.113981, -1.494644});
-	traj1.executePath(path1);
+	float_path1.push_back({0.503454, 0.359961, 1.902053, -3.083623, 1.113981, -1.494644});
+	std::vector<geometry_msgs::Pose> path1;
+	path1.push_back(getPoseFromPathPoint(float_path1.at(0)));
+	path1.push_back(getPoseFromPathPoint(float_path1.at(1)));
+	traj1.executeCartesianPath(path1, "movement towards above fulcrum point 1");
 
 	// Approaching Fulcrum point 1 - Insertion motion Cartesian path
 	// Move in a line segment while approaching fulcrum point and entering body
-	geometry_msgs::Pose fulcrumAbovePose1 = getPoseFromPathPoint(path1.at(path1.size()-1)); // Start insertion trajectory from last target point of previous trajectory
+	geometry_msgs::Pose fulcrumAbovePose1 = path1.at(1); // Start insertion trajectory from last target point of previous trajectory
 	std::vector<geometry_msgs::Pose> path2;
 	path2.push_back(fulcrumAbovePose1);
 	vector<float> fulcrumInsertedPoseFloat1 = {0.514371, 0.217107, 1.610499, -3.083623, 1.113981, -1.494644};
 	geometry_msgs::Pose fulcrumInsertedPose1 = getPoseFromPathPoint(fulcrumInsertedPoseFloat1);
 	path2.push_back(fulcrumInsertedPose1);
-	traj1.executeCartesianPath(path2, "insertion movement");
+	traj1.executeCartesianPath(path2, "trocar 1 insertion movement");
 	// Reverse insertion movement - Remove tool from trocar1
 	std::vector<geometry_msgs::Pose> path3;
 	path3.push_back(fulcrumInsertedPose1);
 	path3.push_back(fulcrumAbovePose1);
-	traj1.executeCartesianPath(path3, "reverse insertion movement");
+	traj1.executeCartesianPath(path3, "trocar 1 reverse insertion movement");
 
 	// Go above fulcrum point 2
 	std::vector<geometry_msgs::Pose> path4;
@@ -64,12 +67,12 @@ int main(int argc, char** argv)
 	vector<float> fulcrumInsertedPoseFloat2 = {0.517311, -0.011860, 1.706976, -2.972108, 1.421710, -1.378877};
 	geometry_msgs::Pose fulcrumInsertedPose2 = getPoseFromPathPoint(fulcrumInsertedPoseFloat2);
 	path5.push_back(fulcrumInsertedPose2);
-	traj1.executeCartesianPath(path5, "insertion movement");
+	traj1.executeCartesianPath(path5, "trocar 2 insertion movement");
 	// Reverse insertion movement - Remove tool from trocar2
 	std::vector<geometry_msgs::Pose> path6;
 	path6.push_back(fulcrumInsertedPose2);
 	path6.push_back(fulcrumAbovePose2);
-	traj1.executeCartesianPath(path6, "reverse insertion movement");
+	traj1.executeCartesianPath(path6, "trocar 2 reverse insertion movement");
 
 	// Go above fulcrum point 3
 	std::vector<geometry_msgs::Pose> path7;
@@ -77,7 +80,7 @@ int main(int argc, char** argv)
 	vector<float> fulcrumAbovePoseFloat3 = {0.522168, -0.093628, 1.976414, 2.971154, 1.389222, 1.391472};
 	geometry_msgs::Pose fulcrumAbovePose3 = getPoseFromPathPoint(fulcrumAbovePoseFloat3);
 	path7.push_back(fulcrumAbovePose3);
-	traj1.executePath(path7);
+	traj1.executePath(path7, "movement towards above fulcrum point 3");
 
 	// Approaching Fulcrum point 3 - Insertion motion Cartesian path
 	// Move in a line segment while approaching fulcrum point and entering body
@@ -86,12 +89,12 @@ int main(int argc, char** argv)
 	vector<float> fulcrumInsertedPoseFloat3 = {0.531381, -0.042863, 1.694900, 2.971154, 1.389222, 1.391472};
 	geometry_msgs::Pose fulcrumInsertedPose3 = getPoseFromPathPoint(fulcrumInsertedPoseFloat3);
 	path8.push_back(fulcrumInsertedPose3);
-	traj1.executeCartesianPath(path8, "insertion movement");
+	traj1.executeCartesianPath(path8, "trocar 3 insertion movement");
 	// Reverse insertion movement - Remove tool from trocar2
 	std::vector<geometry_msgs::Pose> path9;
 	path9.push_back(fulcrumInsertedPose3);
 	path9.push_back(fulcrumAbovePose3);
-	traj1.executeCartesianPath(path9, "reverse insertion movement");
+	traj1.executeCartesianPath(path9, "trocar 3 reverse insertion movement");
 
 	// Go above fulcrum point 4
 	std::vector<geometry_msgs::Pose> path10;
@@ -99,7 +102,7 @@ int main(int argc, char** argv)
 	vector<float> fulcrumAbovePoseFloat4 = {0.519565, -0.394395, 1.853448, 3.077139, 1.095558, 1.501610};
 	geometry_msgs::Pose fulcrumAbovePose4 = getPoseFromPathPoint(fulcrumAbovePoseFloat4);
 	path10.push_back(fulcrumAbovePose4);
-	traj1.executePath(path10);
+	traj1.executePath(path10, "movement towards above fulcrum point 4");
 
 	// Approaching Fulcrum point 4 - Insertion motion Cartesian path
 	// Move in a line segment while approaching fulcrum point and entering body
