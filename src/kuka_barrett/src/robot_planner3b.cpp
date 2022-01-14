e2a74ef350ce36041082445af874e7982ce02d32
Alexios Karadimos <karadalex@gmail.com>
2022-01-14 22:45:28 +0200

    fix pose orientations of trajectory waypoints

--- src/kuka_barrett/src/robot_planner3b.cpp
+++ src/kuka_barrett/src/robot_planner3b.cpp
@@ -16,6 +16,8 @@
 using namespace std;
 namespace rvt = rviz_visual_tools;
 
+typedef geometry_msgs::PoseWithCovarianceStamped covPose;
+
 
 int main(int argc, char** argv)
 {
@@ -53,7 +55,7 @@ int main(int argc, char** argv)
 	path1.clear();
 	path1.push_back(getPoseFromPathPoint(preparation_path.at(1)));
 	path1.push_back(getPoseFromPathPoint(preparation_path.at(2)));
-	traj1.executeCartesianPath(path1, "movement towards above fulcrum point 1");
+	// traj1.executeCartesianPath(path1, "movement towards above fulcrum point 1");
 
 	// Approaching Fulcrum point 1 - Insertion motion Cartesian path
 	// Move in a line segment while approaching fulcrum point and entering body
@@ -65,38 +67,108 @@ int main(int argc, char** argv)
 	path2.push_back(fulcrumInsertedPose1);
 	traj1.executeCartesianPath(path2, "insertion movement", false);
 
-	// Get transformation matrix of reference frame {F} (Fulcrum reference frame) w.r.t. to the universal reference frame {U}
-	Pose* FPose = new Pose(0.529996, 0.059271, 1.398114, 0, 0.0, -0.271542);
-	// Publish axis of fulcrum reference frame, note the small difference in the rpy angles, because of a permutation of the axes from the end-effector to the TCP
-	visual_tools.publishAxisLabeled(getPoseFromPathPoint({0.529996, 0.059271, 1.398114, -0.271542, 0.0, 0}), "Fulcrum2", rvt::MEDIUM);
-	visual_tools.trigger();
-	Eigen::Matrix4d U_T_F = FPose->pose;
-	Eigen::Matrix4d T_7_TCP = Eigen::Matrix4d::Zero(4, 4);
-	T_7_TCP(0, 2) = 1; T_7_TCP(1, 0) = 1;
-	T_7_TCP(2, 1) = 1; T_7_TCP(3, 3) = 1;
-	Eigen::Matrix4d T_TCP_7 = T_7_TCP.inverse();
-
 	Eigen::Vector3f start, end;
 	// Initialize vector with known values https://eigen.tuxfamily.org/dox/group__TutorialAdvancedInitialization.html
 	// values are given in x, y, z order
 	// points that define a line segment that is invariant under the fulcrum transformation
 	// start << 0.0, 0.0, -0.1; // Coordinates of start point of the line segment
 	// end << 0.0, 0.0, -0.3; // Coordinates of start point of the line segment
-	start << -0.1, -0.1, -0.1; // Coordinates of start point of the line segment
-	end << 0.1, 0.1, -0.2; // Coordinates of start point of the line segment
+	start << 0.0, 0.000001, -0.1; // Coordinates of start point of the line segment
+	end << 0.0, 0.1, -0.1; // Coordinates of start point of the line segment
 	LineSegTrajectory* lineSegTrajectory = new LineSegTrajectory(start, end);
-	vector<geometry_msgs::Pose> line_seg_waypoints = lineSegTrajectory->getCartesianWaypoints(10, U_T_F, T_TCP_7);
-	vector<geometry_msgs::Pose> transformed_waypoints = fulcrumEffectTransformation(line_seg_waypoints, 0.4, U_T_F, T_TCP_7);
+	vector<geometry_msgs::Pose> line_seg_waypoints = lineSegTrajectory->getCartesianWaypoints(10);
+	vector<geometry_msgs::Pose> transformed_waypoints = fulcrumEffectTransformation(line_seg_waypoints, 0.4);
+
+	boost::shared_ptr<covPose const> sharedPoseMsg;
+	covPose pose_msg;
+	sharedPoseMsg = ros::topic::waitForMessage<covPose>("fulcrum/estimated/frame2", node_handle);
+	if(sharedPoseMsg != nullptr){
+		pose_msg = *sharedPoseMsg;
+	}
+	auto fp = pose_msg.pose.pose;
+	auto _p = fp.position;
+	tf2::Vector3 pa = tf2::Vector3(_p.x, _p.y, _p.z);
+	auto _q = fp.orientation;
+	tf2::Quaternion qa = tf2::Quaternion(_q.x, _q.y, _q.z, _q.w);
+	tf2::Quaternion rot1 = tf2::Quaternion();
+	rot1.setRPY(0, M_PI_2, 0);
+
+	tf2::Matrix3x3 identity3x3 = tf2::Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+	vector<geometry_msgs::Pose> line_seg_waypoints2, transformed_waypoints2, transformed_waypoints3;
+	for (auto line_seg_pose: transformed_waypoints) {
+		geometry_msgs::Pose new_pose;
+		auto cp = line_seg_pose.position;
+		double px = line_seg_pose.position.x;
+		double py = line_seg_pose.position.y;
+		double pz = line_seg_pose.position.z;
+		tf2::Vector3 dxa = tf2::Vector3(px, py, pz);
+		double th = atan2(sqrt(px*px + py*py), pz);
+		double phi = atan2(py, px);
+		tf2::Quaternion rot2 = tf2::Quaternion();
+		rot2.setRPY(M_PI-phi, 0, 0);
+		tf2::Quaternion rot3 = tf2::Quaternion();
+		rot3.setRPY(0, M_PI_2, 0);
+		tf2::Quaternion rot4 = tf2::Quaternion();
+		rot4.setRPY(0, 0, M_PI_2+th);
+		tf2::Transform tfa = tf2::Transform(qa, pa) * tf2::Transform(rot2, dxa) * tf2::Transform(rot3, tf2::Vector3(0, 0, 0)) * tf2::Transform(rot4, tf2::Vector3(0, 0, 0));
+		tf2::toMsg(tfa, new_pose);
+		transformed_waypoints2.push_back(new_pose);
+	}
+
+	for (auto line_seg_pose: transformed_waypoints) {
+		geometry_msgs::Pose new_pose;
+		auto cp = line_seg_pose.position;
+		double px = line_seg_pose.position.x;
+		double py = line_seg_pose.position.y;
+		double pz = line_seg_pose.position.z;
+		tf2::Vector3 dxa = tf2::Vector3(px, py, pz);
+		double th = atan2(sqrt(px*px + py*py), pz);
+		double phi = atan2(py, px);
+		// double xx = cos(th)*cos(phi); double xy = cos(th)*sin(phi); double xz = -sin(th);
+		// double yx = -sin(phi); double yy = cos(phi); double yz = 0;
+		// double zx = sin(th)*cos(phi); double zy = sin(th)*sin(phi); double zz = cos(th);
+		// tf2::Matrix3x3 orientation = tf2::Matrix3x3(xx, xy, xz, yx, yy, yz, zx, zy, zz);
+		tf2::Quaternion rot2 = tf2::Quaternion();
+		rot2.setRPY(M_PI-phi, 0, 0);
+		tf2::Quaternion rot3 = tf2::Quaternion();
+		rot3.setRPY(0, M_PI_2, 0);
+		tf2::Quaternion rot4 = tf2::Quaternion();
+		rot4.setRPY(0, 0, M_PI_2+th);
+		tf2::Transform tfa = tf2::Transform(qa, pa) * tf2::Transform(rot2, dxa) * tf2::Transform(rot3, tf2::Vector3(0, 0, 0)) * tf2::Transform(rot4, tf2::Vector3(0, 0, 0)) * tf2::Transform(identity3x3, tf2::Vector3(-0.05, 0, -0.094 - 0.022));
+		tf2::toMsg(tfa, new_pose);
+		transformed_waypoints3.push_back(new_pose);
+	}
+
+	for (auto line_seg_pose: line_seg_waypoints) {
+		geometry_msgs::Pose new_pose;
+		auto cp = line_seg_pose.position;
+		double px = line_seg_pose.position.x;
+		double py = line_seg_pose.position.y;
+		double pz = line_seg_pose.position.z;
+		tf2::Vector3 dxa = tf2::Vector3(px, py, pz);
+		double th = atan2(sqrt(px*px + py*py), pz);
+		double phi = atan2(py, px);
+		tf2::Quaternion rot2 = tf2::Quaternion();
+		rot2.setRPY(M_PI-phi, 0, 0);
+		tf2::Quaternion rot3 = tf2::Quaternion();
+		rot3.setRPY(0, M_PI_2, 0);
+		tf2::Quaternion rot4 = tf2::Quaternion();
+		rot4.setRPY(0, 0, -M_PI_2-th);
+		tf2::Transform tfa = tf2::Transform(qa, pa) * tf2::Transform(rot2, dxa) * tf2::Transform(rot3, tf2::Vector3(0, 0, 0)) * tf2::Transform(rot4, tf2::Vector3(0, 0, 0));
+		tf2::toMsg(tfa, new_pose);
+		line_seg_waypoints2.push_back(new_pose);
+	}
 
 	// Path to circle
 	std::vector<geometry_msgs::Pose> path3;
 	path3.push_back(fulcrumInsertedPose1);
-	path3.push_back(transformed_waypoints.at(0));
+	path3.push_back(transformed_waypoints3.at(0));
 
 	traj1.executeCartesianPath(path3, "Path approaching line segment start", false);
-	traj1.executeCartesianPath(transformed_waypoints, "Line Segment transformed Trajectory", false);
+	traj1.executeCartesianPath(transformed_waypoints3, "Line Segment transformed Trajectory", true);
 
-	traj1.visualizeCartesianPath(line_seg_waypoints, "Original taskspace line segment trajectory", false);
+	traj1.visualizeCartesianPath(transformed_waypoints2, "Fulcrum transformed line segment trajectory", true);
+	traj1.visualizeCartesianPath(line_seg_waypoints2, "Original taskspace line segment trajectory", true);
 
 	ros::shutdown();
 	return 0;
